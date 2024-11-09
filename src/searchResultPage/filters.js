6269495ea296ba361a09b80c0c098d477fa4599d
trajkan <trajkan@gmail.com>
2024-11-09 22:07:35 +0100

    adding filters for date and occupancy

--- src/searchResultPage/filters.js
+++ src/searchResultPage/filters.js
@@ -1,13 +1,14 @@
 export function filteredRooms(rooms, fromDate, toDate, numPersons){
     let filteredRooms = rooms;
-    filteredRooms = filterByDate(filteredRooms, fromDate, toDate);
     filteredRooms = filterByOccupancy(filteredRooms, numPersons);
+    filteredRooms = filterByDate(filteredRooms, fromDate, toDate);
     return filteredRooms;
 }
 
 function filterByDate(rooms, fromDate, toDate){
     const filteredRooms = rooms.filter(room => {
         return room.bookedDates.every(date => {
+            console.log('dateFilter',date, fromDate, toDate, date < fromDate || date > toDate);
             return date < fromDate || date > toDate;
         });
     });
